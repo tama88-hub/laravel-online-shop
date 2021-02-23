dc05ffddb4dd0cb998d5763ce7b79fece2f7783a
tama88-hub <cispradmn@gmail.com>
2021-02-23 08:20:39 +0700

    Login Page With Session Done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
+
+//login controller
+Route::post("/login", [UserController::class, 'login']);
+
+//product controller 
+Route::get("/", [ProductController::class, 'index']);
